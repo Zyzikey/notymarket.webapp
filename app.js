ed8769d5f0ace392555f311ea760fc2aa1bcd230
Zyzikey <scihex@gmail.com>
2024-03-21 01:18:28 +1000

    JS
    Best working of fields with memory and dictionaries

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 
+//import { MARKS } from "./modelCatalog.js";
+
 document.addEventListener('DOMContentLoaded', function() {
     updateFields(); 
     updateMarks();
+    updateModels();
 });
 
 function updateFields() {
@@ -20,27 +23,20 @@ function updateFields() {
 
 function updateMarks() {
     const select = document.getElementById('marksSelector');
-    const options = ['AC','Acura','AITO','Aiways','Alfa Romeo','Alpina','Arcfox','Aro','Asia',
-    'Aston Martin','AudiAvatr','BAIC','Baojun','Barkas','BAW','Bentley','BMW','Borgward','Brilliance','Bugatti','Buick','BYD','Cadillac',
-    'Changan','Changhe','Chery','Chevrolet','Chrysler','Ciimo','Citroen','Cupra','Dacia','Dadi','Daewoo','Daihatsu','Daimler','Datsun',
-    'Dayun','DeLorean','Denza','Derways','Dodge','Dongfeng','DW Hower','Eagle','Evolute','EXEED','FAW','Ferrari','Fiat','Fisker','Ford',
-    'Forthing','Foton','GAC','Geely','Genesis','Geo','GMC','Great Wall','Hafei','Haima','Haval','Hawtai','HiPhi','onda','Hongqi','Hozon',
-    'Hummer','Hyundai','IM Motors','Infiniti','Iran Khodro','Isuzu','IVECO','JAC','Jaecoo','Jaguar','Jeep','Jetour','Jetta','Jinbei',
-    'JMC','Kaiyi','Karma','KG Mobility','Kia','Koenigsegg','Kuayue','Lamborghini','Lancia','Land Rover','Landwind','Leapmotor','Lexus',
-    'Li','Lifan','Lincoln','Livan','Lotus','Lucid','Luxgen','Lynk &amp; Co','M-Hero','Marussia','Maserati','Maxus','Maybach','Mazda',
-    'McLaren','Mercedes-Benz','Mercury','MG','MINI','Mitsubishi','Mitsuoka','Nio','Nissan','Oldsmobile','OMODA','Opel','ORA','Oshan',
-    'Oting','Pagani','Perodua','Peugeot','Plymouth','Polar Stone','Polestar','Pontiac','Porsche','Proton','Qingling','Radar','RAM',
-    'Ravon','Renault','Renault Samsung','Rimac','Rising Auto','Rivian','Roewe','Rolls-Royce','Rover','Saab','SAIPA','Saturn','Scion',
-    'SEAT','Seres','Shineray','Shuanghuan','Skoda','Skywell','Smart','Solaris','Sollers','Soueast','SsangYong','Subaru','Suzuki','SWM',
-    'Tank','TATA','Tesla','Tianma','Tianye','Toyota','Venucia','VGV','Volkswagen','Volvo','Vortex','Voyah','Weltmeister','WEY',
-    'Wiesmann','Wuling','Xcite','Xiaomi','Xin Kai','Xpeng','Zeekr','Zotye','ZX','Амберавто','Амбертрак','Аурус','Богдан','ГАЗ',
-    'Донинвест','ЗАЗ','ЗИЛ','ЗиС','ИЖ','Лада','ЛуАЗ','Москвич','Прочие авто','РАФ','ТагАЗ','УАЗ'];
+    const options = MARKS;
+
+    const none_option = document.createElement('option'); // Создаем новый элемент <option>
+    none_option.text = '[- Не указывать -]'; // Устанавливаем текст для <option>
+    select.appendChild(none_option); // Добавляем <option> в <select>
+
     options.forEach(optionText => {
         const option = document.createElement('option'); // Создаем новый элемент <option>
         option.text = optionText; // Устанавливаем текст для <option>
         select.appendChild(option); // Добавляем <option> в <select>
     });
 }
+
+let last_categorySelector = 0;
 
 document.getElementById('category').addEventListener('change', function() {
     updateFields();
@@ -50,14 +46,51 @@ document.getElementById('category').addEventListener('change', function() {
 
 document.getElementById('category').addEventListener('focus', function() {
     var select = document.getElementById('category');
+    last_categorySelector = select.selectedIndex;
     select.selectedIndex = -1;
 });
 
-document.getElementById('category').addEventListener('click', function() {
+document.getElementById('category').addEventListener('focusout', function() {
     var select = document.getElementById('category');
-    console.log(select.hasAttribute('open'))
-    if (select.hasAttribute('open'))
-        select.blur();
+    if (select.selectedIndex == -1)
+        select.selectedIndex = last_categorySelector;
+});
+
+function updateModels() {
+    const marksSelector = document.getElementById('marksSelector');
+    const modelsSelector = document.getElementById('modelsSelector');
+    const options = ALL_MARKS_MODELS[marksSelector.selectedIndex - 1];
+    modelsSelector.innerHTML = '';
+
+    const none_option = document.createElement('option'); // Создаем новый элемент <option>
+    none_option.text = '[- Не указывать -]'; // Устанавливаем текст для <option>
+    modelsSelector.appendChild(none_option); // Добавляем <option> в <select>
+
+    options.forEach(optionText => {
+        const option = document.createElement('option'); // Создаем новый элемент <option>
+        option.text = optionText; // Устанавливаем текст для <option>
+        modelsSelector.appendChild(option); // Добавляем <option> в <select>
+    });
+}
+
+document.getElementById('marksSelector').addEventListener('change', function() {
+    updateModels();
+    var select = document.getElementById('marksSelector');
+    select.blur();
+});
+
+let last_marksSelector = 0;
+
+document.getElementById('marksSelector').addEventListener('focus', function() {
+    var select = document.getElementById('marksSelector');
+    last_marksSelector = select.selectedIndex;
+    select.selectedIndex = -1;
+});
+
+document.getElementById('marksSelector').addEventListener('focusout', function() {
+    var select = document.getElementById('marksSelector');
+    if (select.selectedIndex == -1)
+        select.selectedIndex = last_marksSelector;
 });
 
 
